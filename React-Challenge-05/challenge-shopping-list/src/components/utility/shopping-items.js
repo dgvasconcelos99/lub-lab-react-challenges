fd873a55cf1b8f2627120e79eaf939588c411f39
dgvasconcelos99 <douglas.luby.ext@b2egroup.com.br>
2020-12-11 13:52:18 -0300

    refactor: optimizing shopping list files

---
+++ React-Challenge-05/challenge-shopping-list/src/components/utility/shopping-items.js
@@ -0,0 +1,19 @@
+const shoppingItems = [
+  {
+    name: 'bread',
+    quantity: 2,
+    price: 5.5
+  },
+  {
+    name: 'milk',
+    quantity: 2,
+    price: 3
+  },
+  {
+    name: 'egg',
+    quantity: 30,
+    price: 0.5
+  },
+]
+
+export default shoppingItems
--- React-Challenge-05/challenge-shopping-list/src/containers/App.js
+++ React-Challenge-05/challenge-shopping-list/src/containers/App.js
@@ -3,38 +3,23 @@ import React, { useEffect, useState } from 'react';
 import SelectColor from '../components/SelectColor/SelectColor';
 import ShoppingList from '../components/ShoppingList/ShoppingList';
 import colors from '../components/utility/colors'
+import shoppingItems from '../components/utility/shopping-items'
 
 const app = () => {
+  const [shoppingList, setShoppingList] = useState([])
   const [listColors, setListColors] = useState([])
   const [selectedColor, setSelectedColor] = useState('#282828')
   const [selectedTextColor, setSelectedTextColor] = useState('#fff')
   
   useEffect(() => {
     setListColors(colors)
+    setShoppingList(shoppingItems)
   }, [])
 
   const handleSelectedColors = (color, textColor) => {
     setSelectedColor(color)
     setSelectedTextColor(textColor)
   }
-
-  const shoppingItems = [
-    {
-      name: 'bread',
-      quantity: 2,
-      price: 5.5
-    },
-    {
-      name: 'milk',
-      quantity: 3,
-      price: 3
-    },
-    {
-      name: 'egg',
-      quantity: 30,
-      price: 0.5
-    },
-  ]
 
   return(
     <div>
@@ -43,7 +28,7 @@ const app = () => {
         textColor={
           selectedTextColor
         }
-        shoppingItems={shoppingItems}
+        shoppingItems={shoppingList}
       />
       <SelectColor 
         listColors={listColors}
